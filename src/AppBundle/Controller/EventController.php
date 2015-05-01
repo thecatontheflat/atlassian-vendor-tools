34216a9aa867a125b23b688a48a0acc50e26af24
Vitaliy Zurian <vitaliy.zurian@gmail.com>
2015-05-01 21:15:14 +0200

    events

---
+++ src/AppBundle/Controller/EventController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class EventController extends Controller
+{
+    /**
+     * @Route("/event", name="event")
+     */
+    public function indexAction()
+    {
+        $events = $this->container->getParameter('events');
+
+        return $this->render(':event:index.html.twig', ['events' => $events]);
+    }
+}
