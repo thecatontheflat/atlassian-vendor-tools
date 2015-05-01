6ea9021d62971efe2ff19df19cf9142a6d88a1eb
Vitaliy Zurian <vitaliy.zurian@gmail.com>
2015-05-01 21:52:48 +0200

    Scheduling events

--- src/AppBundle/Command/ScheduleEventsCommand.php
+++ src/AppBundle/Command/ScheduleEventsCommand.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Command;
 
+use AppBundle\Entity\ScheduledEvent;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -16,16 +17,39 @@ class ScheduleEventsCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $events = $this->getContainer()->getParameter('events');
-        $repository = $this->getContainer()->get('doctrine')->getRepository('AppBundle:License');
+        $licenseRepo = $this->getContainer()->get('doctrine')->getRepository('AppBundle:License');
+        $scheduledEventRepo = $this->getContainer()->get('doctrine')->getRepository('AppBundle:ScheduledEvent');
+        $em = $this->getContainer()->get('doctrine')->getManager();
 
         foreach ($events as $event) {
-            foreach ($repository->findByEvent($event) as $license) {
+            foreach ($licenseRepo->findByEvent($event) as $license) {
+                $existing = $scheduledEventRepo->findOneBy([
+                    'licenseId' => $license->getLicenseId(),
+                    'addonKey' => $license->getAddonKey(),
+                    'name' => $event['name']
+                ]);
 
+                if ($existing) {
+                    $message = sprintf('%s: %s skipped', $license->getLicenseId(), $event['name']);
+                    $output->writeln($message);
+                    continue;
+                }
+
+                $scheduledEvent = new ScheduledEvent();
+                $scheduledEvent
+                    ->setAddonKey($license->getAddonKey())
+                    ->setLicenseId($license->getLicenseId())
+                    ->setName($event['name'])
+                    ->setStatus('new');
+
+                $em->persist($scheduledEvent);
                 $message = sprintf('%s: %s scheduled', $license->getLicenseId(), $event['name']);
 
                 $output->writeln($message);
             }
         }
+
+        $em->flush();
 
         $output->writeln('Done');
     }
