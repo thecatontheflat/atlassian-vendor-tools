29a3f40f4c328472a4ed8b83a26942ebc84e79cb
Vitaliy Zurian <vitaliy.zurian@gmail.com>
2015-05-03 15:45:31 +0200

    Send events command

--- src/AppBundle/Command/SendScheduledEventsCommand.php
+++ src/AppBundle/Command/SendScheduledEventsCommand.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Command;
 
+use AppBundle\Entity\License;
 use AppBundle\Entity\ScheduledEvent;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
@@ -26,25 +27,7 @@ class SendScheduledEventsCommand extends ContainerAwareCommand
 
         foreach ($events as $event) {
             $license = $licenseRepo->findOneBy(['licenseId' => $event->getLicenseId(), 'addonKey' => $event->getAddonKey()]);
-            $recipient = $this->getContainer()->getParameter('vendor_email');
-            $bcc = $this->getContainer()->getParameter('vendor_email');
-            $content = [
-                $this->contentHash('FNAME', $license->getTechContactName()),
-                $this->contentHash('ADDON_NAME', $license->getAddonName()),
-                $this->contentHash('ADDON_KEY', $license->getAddonKey()),
-                $this->contentHash('LICENSE_ID', $license->getLicenseId()),
-                $this->contentHash('LICENSE_START_DATE', $license->getStartDate()->format('Y-m-d')),
-                $this->contentHash('LICENSE_END_DATE', $license->getEndDate()->format('Y-m-d'))
-            ];
-
-            $message = [
-                'subject' => null,
-                'from_email' => null,
-                'from_name' => null,
-                'to' => [['email' => $recipient]],
-                'bcc_address' => $bcc,
-                'global_merge_vars' => $content
-            ];
+            $message = $this->prepareMessage($license);
 
             try {
                 $mandrill->messages->sendTemplate($event->getName(), [], $message, true);
@@ -70,5 +53,32 @@ class SendScheduledEventsCommand extends ContainerAwareCommand
             'name' => $key,
             'content' => $value
         ];
+    }
+
+
+    private function prepareMessage(License $license)
+    {
+        $recipient = $this->getContainer()->getParameter('vendor_email');
+        $bcc = $this->getContainer()->getParameter('vendor_email');
+
+        $content = [
+            $this->contentHash('FNAME', $license->getTechContactName()),
+            $this->contentHash('ADDON_NAME', $license->getAddonName()),
+            $this->contentHash('ADDON_KEY', $license->getAddonKey()),
+            $this->contentHash('LICENSE_ID', $license->getLicenseId()),
+            $this->contentHash('LICENSE_START_DATE', $license->getStartDate()->format('Y-m-d')),
+            $this->contentHash('LICENSE_END_DATE', $license->getEndDate()->format('Y-m-d'))
+        ];
+
+        $message = [
+            'subject' => 'TESTING - '.$license->getAddonName(),
+            'from_email' => null,
+            'from_name' => null,
+            'to' => [['email' => $recipient]],
+            'bcc_address' => $bcc,
+            'global_merge_vars' => $content
+        ];
+
+        return $message;
     }
 }
