085f21a3203011861e9d8612778a239ca4a90a89
Vitaliy Zurian <vitaliy.zurian@gmail.com>
2015-05-08 19:18:57 +0200

    Events

--- src/AppBundle/Controller/EventController.php
+++ src/AppBundle/Controller/EventController.php
@@ -2,9 +2,13 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\ScheduledEvent;
+use AppBundle\Form\EventType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Event;
+
 
 class EventController extends Controller
 {
@@ -18,6 +22,71 @@ class EventController extends Controller
 
         return $this->render(':event:index.html.twig', [
             'events' => $events
+        ]);
+    }
+
+    /**
+     * @Route("/event/new", name="event_new")
+     * @Method("GET")
+     */
+    public function newAction()
+    {
+        $entity = new Event();
+        $form = $this->createForm(new EventType(), $entity, [
+            'action' => $this->generateUrl('event_new_post'),
+            'method' => 'POST'
+        ]);
+
+        return $this->render(':event:new.html.twig', [
+            'entity' => $entity,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     *
+     * @Route("/event/new", name="event_new_post")
+     * @Method("POST")
+     */
+    public function createAction(Request $request)
+    {
+        $entity = new Event();
+        $form = $this->createForm(new EventType(), $entity, [
+            'action' => $this->generateUrl('event_new'),
+            'method' => 'POST'
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('event_show', ['id' => $entity->getId()]));
+        }
+
+        return $this->render(':event:new.html.twig', [
+            'entity' => $entity,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/event/{id}", name="event_show")
+     * @Method("GET")
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $entity = $em->getRepository('AppBundle:Event')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Template entity.');
+        }
+
+        return $this->render(':event:show.html.twig', [
+            'entity' => $entity,
         ]);
     }
 }
---
+++ src/AppBundle/Form/EventType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class EventType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('addonKey')
+            ->add('template')
+            ->add('licenseType')
+            ->add('licenseField')
+            ->add('shiftDays')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Event'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'appbundle_event';
+    }
+}
