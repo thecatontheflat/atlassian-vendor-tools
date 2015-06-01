68665d5a1e5696f56eebe98e2f5a6298d11c5235
Vitaliy Zurian <vitaliy.zurian@gmail.com>
2015-06-01 21:45:30 +0200

    test emails

--- src/AppBundle/Controller/EventController.php
+++ src/AppBundle/Controller/EventController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\DrillSchemaEvent;
+use AppBundle\Entity\License;
 use AppBundle\Form\DrillSchemaEventType;
 use AppBundle\Form\EventType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -69,5 +70,55 @@ class EventController extends Controller
         return $this->render(':event:edit.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/event/{id}/send-test", name="event_send_test")
+     */
+    public function sendTestAction(DrillSchemaEvent $event)
+    {
+        $license = $this->getDoctrine()->getRepository('AppBundle:License')->findOneBy(['licenseId' => 'SEN-3253462']);
+        $mandrill = $this->get('app.mandrill');
+        $message = $this->prepareMessage($license, $event);
+        $mandrill->messages->send($message, true);
+
+        return $this->redirectToRoute('events');
+    }
+
+    private function prepareMessage(License $license, DrillSchemaEvent $event)
+    {
+        $recipients = [['email' => $this->container->getParameter('vendor_email')]];
+
+        $html = $event->getEmailTemplate();
+        $subject = $event->getEmailSubject();
+
+        $this->replaceTemplateVariables($html, $license);
+        $this->replaceTemplateVariables($subject, $license);
+
+        $message = [
+            'subject' => $subject,
+            'from_email' => $event->getEmailFromEmail(),
+            'from_name' => $event->getEmailFromName(),
+            'to' => $recipients,
+            'html' => $html
+        ];
+
+        return $message;
+    }
+
+    private function replaceTemplateVariables(&$html, License $license)
+    {
+        $mapping = [
+            '%_TECH_CONTACT_%' => $license->getTechContactName(),
+            '%_ADDON_NAME_%' => $license->getAddonName(),
+            '%_ADDON_KEY_%' => $license->getAddonKey(),
+            '%_LICENSE_ID_%' => $license->getLicenseId(),
+            '%_LICENSE_START_DATE_%' => $license->getStartDate()->format('Y-m-d'),
+            '%_LICENSE_END_DATE_%' => $license->getEndDate()->format('Y-m-d'),
+        ];
+
+        foreach ($mapping as $token => $replacement) {
+            $html = str_replace($token, $replacement, $html);
+        }
     }
 }
