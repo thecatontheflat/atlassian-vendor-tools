252ada252e98ee786443db27c8c06cd21a91fd14
Vitaliy Zurian <vitaliy.zurian@gmail.com>
2015-10-14 21:28:41 +0200

    Some tests

--- src/AppBundle/Tests/Controller/DashboardControllerTest.php
+++ src/AppBundle/Tests/Controller/DashboardControllerTest.php
@@ -6,11 +6,23 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DashboardControllerTest extends WebTestCase
 {
-    public function testHomepage()
+    public function testHomepageAndWidgets()
     {
         $client = $this->createClient();
-        $client->request('GET', '/');
+        $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $starter = $crawler->filter('#started-today tbody tr td')->first()->text();
+        $starter = trim($starter);
+        $this->assertEquals('SEN-111111', $starter);
+
+        $expiringSoon = $crawler->filter('#expiring-soon tbody tr td')->first()->text();
+        $expiringSoon = trim($expiringSoon);
+        $this->assertEquals('SEN-222222', $expiringSoon);
+
+        $topCustomer = $crawler->filter('#top-customers td')->first()->text();
+        $topCustomer = trim($topCustomer);
+        $this->assertEquals('SEN-222222', $topCustomer);
     }
 }
