76e8f23b969b763a5907b91506d3f21ef9d3ebc1
Zacklane <vdorozhenko@gmail.com>
2017-09-05 18:24:15 -0500

    User entity established to use doctrine ORM.

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,26 @@
+<?php
+// src/AppBundle/Entity/User.php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="avt_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
