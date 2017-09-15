4af2b1b910bf373e35c3938d32da8d9e62b8f36a
Zacklane <vdorozhenko@gmail.com>
2017-09-15 12:29:44 +0200

    FOSUserBundle removed from kernel, in routing config and apllication main config.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
-            new FOS\UserBundle\FOSUserBundle(),
             new AppBundle\AppBundle(),
         );
 
