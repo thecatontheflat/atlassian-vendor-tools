8718ca3c34f81bb390334eaacef7caa1e367cee1
Zacklane <vdorozhenko@gmail.com>
2017-09-22 17:21:17 +0300

    All the commented lines removed from this controller code.

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -1,5 +1,4 @@
 <?php
-// src/AppBundle/Controller/SecurityController.php
 
 namespace AppBundle\Controller;
 
@@ -15,10 +14,8 @@ class SecurityController extends Controller
     {
         $authenticationUtils = $this->get('security.authentication_utils');
 
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render(
